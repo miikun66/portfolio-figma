43fa0e08cb730eca11eb3ab7388f8e40ba08ba94
Michael <michael.jambi49@gmail.com>
2025-08-17 15:46:17 +0700

    tes

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h2 className="text-[var(--primary)] text-2xl font-[var(--font-mono)]">Hello ini adalah halaman utama</h2>
+      <p>halo nama saya </p>
     </div>
   );
 }
